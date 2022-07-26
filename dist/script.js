98bfe565beaf6c15dc774ec0d74f0e3b0ffc84c8
Aleksi Toivonen <aptoivonen@mail.com>
2022-07-26 16:52:58 +0300

    Change script a bit

--- dist/script.js
+++ dist/script.js
@@ -2,7 +2,8 @@ const jp3 = document.getElementById("jp3");
 
 const tl = gsap.timeline();
 
-tl.to(jp3, { duration: 3, x: -50, y: -50 })
-  .to(jp3, { duration: 3, x: 0, y: -100 })
-  .to(jp3, { duration: 3, x: 30, y: -130 })
-  .to(jp3, { duration: 3, x: 45, y: -160 });
+tl.to(jp3, { duration: 10, x: 0, y: -250 }).to(jp3, {
+  duration: 3,
+  x: 30,
+  y: -350,
+});
