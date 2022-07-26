d26a55b9be51f01c6a16db87017e56891c17d33e
Aleksi Toivonen <aptoivonen@mail.com>
2022-07-26 14:51:09 +0300

    Redo animation

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,8 @@
-const jp3up = document.getElementById("symbol-jp3up");
+const jp3 = document.getElementById("symbol-jp3");
 
 const tl = gsap.timeline();
 
-tl.to(jp3up, { duration: 3, x: -50, y: -50 })
-  .to(jp3up, { duration: 3, x: 0, y: -100 })
-  .to(jp3up, { duration: 3, x: 30, y: -130 })
-  .to(jp3up, { duration: 3, x: 45, y: -160 });
+tl.to(jp3, { duration: 3, x: -50, y: -50 })
+  .to(jp3, { duration: 3, x: 0, y: -100 })
+  .to(jp3, { duration: 3, x: 30, y: -130 })
+  .to(jp3, { duration: 3, x: 45, y: -160 });
