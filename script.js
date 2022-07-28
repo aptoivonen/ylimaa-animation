1a1dd36548cf86baa12f9e8f6c20dc6b1941c6f4
Aleksi Toivonen <aptoivonen@mail.com>
2022-07-28 18:14:07 +0300

    Updates

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Add new units here
 const UNITS = [
   { id: "ghq218", data: "svg/hq-218.svg" },
+  { id: "ghq218I", data: "svg/hq-218-I.svg" },
   { id: "ghq218II", data: "svg/hq-218-II.svg" },
   { id: "g218I", data: "svg/218-I.svg" },
   { id: "g218II", data: "svg/218-II.svg" },
@@ -45,6 +46,7 @@ const START_DATE_STRING = "October 7, 1944 06:00:00";
 const xOffset = -25;
 const yOffset = -3;
 
+// prettier-ignore
 const animationSteps = [
   {
     jp2: { x: 340, y: 970, opacity: 0 },
@@ -52,11 +54,12 @@ const animationSteps = [
     jp4: { x: 360, y: 900, opacity: 0 },
     jp5: { x: 1130, y: 1080 },
     ghq218: { x: 770, y: 350 },
+    ghq218I: { x: 790, y: 570 },
     ghq218II: { x: 630, y: 210 },
     g218I: { x: 800, y: 730 },
     g218II: { x: 560, y: 196 },
     g218III: { x: 1190, y: 910 },
-    gII8: { x: 180, y: 491 },
+    gII8: { x: 167, y: 465 },
     g21816: { x: 1080, y: 970 },
   }, // set up
   { jp3: { x: 340, y: 990 } },
@@ -68,14 +71,10 @@ const animationSteps = [
   { jp2: { x: 375, y: 890 }, jp3: { x: 289, y: 662 } },
   { jp2: { x: 523, y: 770 }, jp3: { x: 331, y: 587 } },
   { jp2: { x: 538, y: 751 }, jp3: { x: 353, y: 557 } },
-  { jp2: { x: 583, y: 717 }, jp3: { x: 338, y: 518 } },
-  { jp2: { x: 619, y: 690 }, jp3: { x: 353, y: 495 } },
+  { jp2: { x: 583, y: 717 }, jp3: { x: 338, y: 518 }, gII8: { x: 272, y: 480 }, },
+  { jp2: { x: 619, y: 690 }, jp3: { x: 353, y: 495 }, gII8: { x: 205, y: 467 }, },
   { jp3: { x: 344, y: 542 }, g218I: { x: 689, y: 721 } }, // 7.10 klo 18
-  {
-    jp2: { x: 549, y: 662 },
-    jp3: { x: 341, y: 593 },
-    g218I: { x: 800, y: 730 },
-  },
+  { jp2: { x: 549, y: 662 }, jp3: { x: 341, y: 593 }, g218I: { x: 800, y: 730 }, },
   { jp2: { x: 518, y: 686 } },
   {},
   {},
