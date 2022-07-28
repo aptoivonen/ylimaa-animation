6321f3b6fa62d7bdd700b7eaffdc0ddfde385256
Aleksi Toivonen <aptoivonen@mail.com>
2022-07-28 18:00:48 +0300

    Do major overhaul of animation system

--- dist/script.js
+++ dist/script.js
@@ -1,161 +1,203 @@
 // Add new units here
 const UNITS = [
-  { id: "ghq218", data: "svg/hq-218.svg", x: 720, y: 300 },
-  { id: "ghq218II", data: "svg/hq-218-II.svg", x: 560, y: 210 },
-  { id: "g218I", data: "svg/218-I.svg", x: 800, y: 730 },
-  { id: "g218II", data: "svg/218-II.svg", x: 520, y: 130 },
-  { id: "g218III", data: "svg/218-III.svg", x: 1100, y: 830 },
-  { id: "gII8", data: "svg/II-8.svg", x: 80, y: 450 },
-  { id: "g21816", data: "svg/218-16.svg", x: 1010, y: 940 },
-  { id: "jp2", data: "svg/jp2.svg", x: 270, y: 900, opacity: 0 },
-  { id: "jp3", data: "svg/jp3.svg", x: 270, y: 1000 },
-  { id: "jp4", data: "svg/jp4.svg", x: -999, y: -999 },
-  { id: "jp5", data: "svg/jp5.svg", x: 1050, y: 1020 },
+  { id: "ghq218", data: "svg/hq-218.svg" },
+  { id: "ghq218II", data: "svg/hq-218-II.svg" },
+  { id: "g218I", data: "svg/218-I.svg" },
+  { id: "g218II", data: "svg/218-II.svg" },
+  { id: "g218III", data: "svg/218-III.svg" },
+  { id: "gII8", data: "svg/II-8.svg" },
+  { id: "g21816", data: "svg/218-16.svg" },
+  { id: "jp2", data: "svg/jp2.svg" },
+  { id: "jp3", data: "svg/jp3.svg" },
+  { id: "jp4", data: "svg/jp4.svg" },
+  { id: "jp5", data: "svg/jp5.svg" },
 ];
 
-// Populate map with units
-const map = document.getElementById("map");
-// Required by foreignObject creation
-const ns = "http://www.w3.org/2000/svg";
+function populateWithUnits() {
+  // Populate map with units
+  const map = document.getElementById("map");
+  // Required by foreignObject creation
+  const ns = "http://www.w3.org/2000/svg";
 
-UNITS.forEach((unit) => {
-  const newUnitElement = document.createElementNS(ns, "foreignObject");
-  newUnitElement.setAttribute("class", "symbol-wrapper");
-  newUnitElement.setAttribute("x", unit.x);
-  newUnitElement.setAttribute("y", unit.y);
-  const obj = document.createElement("object");
-  obj.className = "unit-symbol";
-  obj.id = unit.id;
-  obj.data = unit.data;
-  obj.type = "image/svg+xml";
-  newUnitElement.appendChild(obj);
-  map.appendChild(newUnitElement);
-});
+  UNITS.forEach((unit) => {
+    const newUnitElement = document.createElementNS(ns, "foreignObject");
+    newUnitElement.setAttribute("class", "symbol-wrapper");
+    // newUnitElement.setAttribute("x", unit.x);
+    // newUnitElement.setAttribute("y", unit.y);
+    newUnitElement.setAttribute("x", 0);
+    newUnitElement.setAttribute("y", 0);
+    const obj = document.createElement("object");
+    obj.className = "unit-symbol";
+    obj.id = unit.id;
+    obj.data = unit.data;
+    obj.type = "image/svg+xml";
+    newUnitElement.appendChild(obj);
+    map.appendChild(newUnitElement);
+  });
+}
+
+populateWithUnits();
 
 // Animation
 const STEP_DURATION = 1;
+const EASE = "none";
 const START_DATE_STRING = "October 7, 1944 06:00:00";
-let step = 1;
+const xOffset = -25;
+const yOffset = -3;
+
 const animationSteps = [
-  { jp3: { x: 0, y: -50 } }, // 7.10 klo 7
-  { jp3: { x: 0, y: -100 } },
-  { jp3: { x: 0, y: -150 } },
-  { jp3: { x: 0, y: -200 } },
-  { jp3: { x: 0, y: -200 }, jp2: { opacity: 1 } }, // 7.10 klo 11
-  { jp3: { x: 0, y: -250 }, jp2: { x: 0, y: -50 } },
+  {
+    jp2: { x: 340, y: 970, opacity: 0 },
+    jp3: { x: 350, y: 1033 },
+    jp4: { x: 360, y: 900, opacity: 0 },
+    jp5: { x: 1130, y: 1080 },
+    ghq218: { x: 770, y: 350 },
+    ghq218II: { x: 630, y: 210 },
+    g218I: { x: 800, y: 730 },
+    g218II: { x: 560, y: 196 },
+    g218III: { x: 1190, y: 910 },
+    gII8: { x: 180, y: 491 },
+    g21816: { x: 1080, y: 970 },
+  }, // set up
+  { jp3: { x: 340, y: 990 } },
+  { jp3: { x: 366, y: 935 } },
+  { jp3: { x: 360, y: 900 } },
+  { jp3: { x: 380, y: 860 } },
+  { jp2: { opacity: 1 } },
+  { jp2: { x: 366, y: 926 }, jp3: { x: 342, y: 755 } }, // 7.10 klo 12
+  { jp2: { x: 375, y: 890 }, jp3: { x: 289, y: 662 } },
+  { jp2: { x: 523, y: 770 }, jp3: { x: 331, y: 587 } },
+  { jp2: { x: 538, y: 751 }, jp3: { x: 353, y: 557 } },
+  { jp2: { x: 583, y: 717 }, jp3: { x: 338, y: 518 } },
+  { jp2: { x: 619, y: 690 }, jp3: { x: 353, y: 495 } },
+  { jp3: { x: 344, y: 542 }, g218I: { x: 689, y: 721 } }, // 7.10 klo 18
+  {
+    jp2: { x: 549, y: 662 },
+    jp3: { x: 341, y: 593 },
+    g218I: { x: 800, y: 730 },
+  },
+  { jp2: { x: 518, y: 686 } },
+  {},
+  {},
+  {}, // 7.10 klo 23
 ];
 
-let tl;
-let intervalId;
-let isPlaying = false;
+const tl = createAnimation(animationSteps);
 
-makeTimeline();
+function createAnimation(animationArr) {
+  const tl = gsap.timeline({
+    defaults: { duration: STEP_DURATION, ease: EASE },
+  });
 
-function makeTimeline() {
-  tl = gsap.timeline({
-    default: { duration: STEP_DURATION },
+  animationArr.forEach((animationEntry, index) => {
+    createAnimationStep(animationEntry, index, tl);
   });
-  UNITS.forEach((unit) => {
-    if ("opacity" in unit) {
-      tl.set("#" + unit.id, { opacity: unit.opacity });
-    }
-  });
+
+  animationEnd(tl);
+  return tl;
 }
 
-function takeStep() {
-  step++;
-  if (!stepsLeft()) return;
-  updateBattleTime();
-  const animationData = animationSteps[step - 2];
-  if (animationData) {
-    Object.entries(animationData).forEach(([unitId, delta]) => {
-      tl.to(
-        "#" + unitId,
-        { x: delta.x, y: delta.y, opacity: delta.opacity },
-        `step-${step}`
-      );
-    });
+function createAnimationStep(data, step, tl) {
+  Object.entries(data).forEach(([unitId, animationVars]) => {
+    const unitSelector = "#" + unitId;
+    let rect;
+    if ("x" in animationVars || "y" in animationVars) {
+      rect = document.querySelector(unitSelector).getBoundingClientRect();
+    }
+    if ("x" in animationVars) {
+      animationVars.x = animationVars.x - Math.floor(rect.width / 2) + xOffset;
+    }
+    if ("y" in animationVars) {
+      animationVars.y = animationVars.y - Math.floor(rect.height / 2) + yOffset;
+    }
+    tl.to(unitSelector, animationVars, `step-${step}`);
+  });
+  // empty step? create a null step
+  if (Object.keys(data).length === 0) {
+    tl.to("body", {}, `step-${step}`);
   }
+  // first step? set up units
+  if (step === 0) {
+    setupComplete(tl);
+  }
+  updateStep(step, tl);
 }
 
-function animationEnd() {
-  clearInterval(intervalId);
-  forwardButton.disabled = true;
-  playButton.classList.add("show");
-  pauseButton.classList.remove("show");
-}
-
-function play() {
-  isPlaying = true;
-  intervalId = setInterval(() => {
-    takeStep();
-    if (!stepsLeft()) {
-      animationEnd();
-      return;
+function setupComplete(tl) {
+  tl.call(() => {
+    if (tl.reversed()) {
+      tl.reverse();
+      tl.pause();
     }
-  }, Math.floor(1000 * STEP_DURATION));
-}
-
-function pause() {
-  isPlaying = false;
-  if (intervalId) clearInterval(intervalId);
-}
-
-function forward() {
-  if (!stepsLeft()) return;
-  takeStep();
-}
-
-function restart() {
-  tl.clear();
-  UNITS.forEach((unit) => {
-    document.getElementById(unit.id).removeAttribute("style");
-    if ("opacity" in unit) {
-      document
-        .getElementById(unit.id)
-        .setAttribute("style", `opacity: ${unit.opacity}`);
-    }
-    tl.set("#" + unit.id, { x: 0, y: 0 });
   });
-  step = 1;
-  updateBattleTime();
-  forwardButton.disabled = false;
 }
 
-function stepsLeft() {
-  return step - 2 < animationSteps.length;
+function updateStep(step, tl) {
+  tl.call(() => {
+    updateBattleTime(step);
+  });
+}
+
+function animationEnd(tl) {
+  tl.pause("step-1");
+  tl.call(() => {
+    endHandler();
+  });
 }
 
 function playHandler() {
-  playButton.classList.remove("show");
-  pauseButton.classList.add("show");
-  if (stepsLeft()) {
-    play();
+  if (tl.reversed()) {
+    tl.reverse();
   }
+  if (tl.paused()) {
+    tl.resume();
+    return;
+  }
+  showPauseButton();
+  tl.play();
 }
 
 function pauseHandler() {
-  playButton.classList.add("show");
-  pauseButton.classList.remove("show");
-  pause();
-}
-
-function forwardHandler() {
-  pauseHandler();
-  if (!stepsLeft()) {
-    forwardButton.disabled = true;
-    return;
-  }
-  takeStep();
+  showPlayButton();
+  if (!tl.paused()) tl.pause();
 }
 
 function restartHandler() {
-  pauseHandler();
-  restart();
+  tl.pause().seek("step-1").resume();
+  if (tl.reversed()) {
+    tl.reverse();
+  }
+  toggleDisablePlayButton(false);
+  showPauseButton();
 }
 
-function updateBattleTime() {
-  battleTime.textContent = formatBattleTime(step - 1);
+function reverseHandler() {
+  tl.reverse();
+  toggleDisablePlayButton(false);
+  showPauseButton();
+}
+
+function endHandler() {
+  showPlayButton();
+  toggleDisablePlayButton(true);
+}
+
+function toggleDisablePlayButton(isDisabled) {
+  playButton.disabled = isDisabled;
+}
+
+function showPlayButton() {
+  playButton.classList.add("show");
+  pauseButton.classList.remove("show");
+}
+
+function showPauseButton() {
+  playButton.classList.remove("show");
+  pauseButton.classList.add("show");
+}
+
+function updateBattleTime(step) {
+  battleTime.textContent = formatBattleTime(step);
 }
 
 function formatBattleTime(step) {
@@ -168,15 +210,15 @@ function formatBattleTime(step) {
 }
 
 // Set up controls
+const restartButton = document.getElementById("restart-btn");
 const playButton = document.getElementById("play-btn");
 const pauseButton = document.getElementById("pause-btn");
-const restartButton = document.getElementById("restart-btn");
-const forwardButton = document.getElementById("step-btn");
+const reverseButton = document.getElementById("reverse-btn");
 const battleTime = document.getElementById("battle-time");
 
+restartButton.addEventListener("click", restartHandler);
 playButton.addEventListener("click", playHandler);
 pauseButton.addEventListener("click", pauseHandler);
-restartButton.addEventListener("click", restartHandler);
-forwardButton.addEventListener("click", forwardHandler);
+reverseButton.addEventListener("click", reverseHandler);
 
-updateBattleTime();
+updateBattleTime(0);
