a9bc3d16d7c00c54d02f49c1cd9b32a7dc490266
Aleksi Toivonen <aptoivonen@mail.com>
2022-07-29 17:45:51 +0300

    Updates

--- js/data.js
+++ js/data.js
@@ -57,7 +57,7 @@ export const UNITS = [
     parentName: "II",
   },
   {
-    id: "g21816",
+    id: "g_218_16",
     type: UNIT_TYPES.mountainInfantry,
     affiliation: AFFILIATION.German,
     echelon: ECHELON.company,
@@ -72,11 +72,27 @@ export const UNITS = [
     unitName: "JP2",
   },
   {
+    id: "jp2_3",
+    type: UNIT_TYPES.infantry,
+    affiliation: AFFILIATION.Finnish,
+    echelon: ECHELON.company,
+    unitName: "3",
+    parentName: "JP2",
+  },
+  {
     id: "jp3",
     type: UNIT_TYPES.infantry,
     affiliation: AFFILIATION.Finnish,
     echelon: ECHELON.battalion,
     unitName: "JP3",
+  },
+  {
+    id: "jp3_1",
+    type: UNIT_TYPES.infantry,
+    affiliation: AFFILIATION.Finnish,
+    echelon: ECHELON.company,
+    unitName: "1",
+    parentName: "JP3",
   },
   {
     id: "jp4",
@@ -98,8 +114,10 @@ export const UNITS = [
 export const ANIMATION_STEPS = [
   {
     jp2: { x: 340, y: 970, opacity: 0 },
+    jp2_3: {x: 518, y: 686 ,opacity: 0},
     jp3: { x: 350, y: 1033 },
-    jp4: { x: 360, y: 900, opacity: 0 },
+    jp3_1: { x: 305, y: 562, opacity: 0 },
+    jp4: { x: 385, y: 897, opacity: 0 },
     jp5: { x: 1130, y: 1080 },
     ghq218: { x: 770, y: 350 },
     ghq218I: { x: 790, y: 570 },
@@ -108,7 +126,7 @@ export const ANIMATION_STEPS = [
     g218II: { x: 560, y: 196 },
     g218III: { x: 1190, y: 910 },
     gII8: { x: 167, y: 465 },
-    g21816: { x: 1080, y: 970 },
+    g_218_16: { x: 1087, y: 992 },
   }, // set up
   { jp3: { x: 340, y: 990 } },
   { jp3: { x: 366, y: 935 } },
@@ -123,9 +141,22 @@ export const ANIMATION_STEPS = [
   { jp2: { x: 619, y: 690 }, jp3: { x: 353, y: 495 }, gII8: { x: 205, y: 467 }, },
   { jp3: { x: 344, y: 542 }, g218I: { x: 689, y: 721 } }, // 7.10 klo 18
   { jp2: { x: 549, y: 662 }, jp3: { x: 341, y: 593 }, g218I: { x: 800, y: 730 }, },
-  { jp2: { x: 518, y: 686 } },
-  {},
+  { jp2: { x: 518, y: 686 }, jp4: {opacity: 1} },
+  { jp4: {x: 397, y: 880}},
   {},
   {}, 
   {}, // 8.10 klo 00
+  {},
+  {},
+  {jp2_3: {opacity: 1}},
+  {jp2_3: {x: 575, y: 628}},
+  {},
+  {}, // 8.10 klo 6.00
+  {},
+  {jp2_3: {x: 692, y: 480}},
+  {},
+  {},
+  {jp3: {x: 315, y: 488}, jp2: {x: 575, y:627}, jp3_1: {opacity: 1}},
+  {jp3_1:{x: 282, y: 554}, jp3: {x: 317, y:459}, jp2: {x:641 , y:535}, gII8: {x: 224, y: 463}}, // 8.10 klo 12.00
+  {gII8: { x: 205, y: 467 }, jp2: {x: 673, y: 483}, jp2_3: {opacity: 0}, jp4: {x: 385, y: 835}}
 ];
